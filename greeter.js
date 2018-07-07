3a93d304cb1a79a53492f226ceb285a24b779d3f
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-07-07 14:23:26 +0200

    try Interfaces

--- greeter.js
+++ greeter.js
@@ -1,6 +1,13 @@
+// function greeter(person: string) {
+//     return "Hello, " + person;
+// }
+//
+// let user = [1, 2, 3]; //"Dominik Palacz";
+//
+// //alert(greeter(user));
+// document.body.innerHTML = greeter(user);
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = [1, 2, 3]; //"Dominik Palacz";
-alert(greeter(user));
+var user = { firstName: 'Dominik', lastName: 'Palacz' };
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,8 +1,21 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+// function greeter(person: string) {
+//     return "Hello, " + person;
+// }
+//
+// let user = [1, 2, 3]; //"Dominik Palacz";
+//
+// //alert(greeter(user));
+// document.body.innerHTML = greeter(user);
+
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-let user = [1, 2, 3] //"Dominik Palacz";
+function greeter(person: Person) {
+    return `Hello, ${person.firstName} ${person.lastName}`;
+}
 
-alert(greeter(user));
+let user = {firstName: 'Dominik', lastName: 'Palacz'};
+
 document.body.innerHTML = greeter(user);
