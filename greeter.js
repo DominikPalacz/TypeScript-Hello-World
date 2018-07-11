fe161e0586886822c4fafb9763e6207c45e76f69
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-07-11 14:55:02 +0200

    try Interface

--- greeter.js
+++ greeter.js
@@ -9,27 +9,43 @@
 //
 // document.addEventListener("DOMContentLoaded", function (event) {
 //     console.log("DOM fully loaded and parsed");
-var Student = /** @class */ (function () {
-    function Student(firstName, middleInitial, lastName) {
-        this.firstName = firstName;
-        this.middleInitial = middleInitial;
-        this.lastName = lastName;
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
-    return Student;
-}());
-function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-var user = new Student('Dominik', 'M', 'Palacz'); //{firstName: 'Dominik', lastName: 'Palacz'};
-//document.body.innerHTML = greeter(user); OK ;)
-var myName = "Dominik";
-var myAge = 41;
-var canVote = true;
-var anything = 'dog';
-anything = 2;
-document.getElementById("tsStuff").innerHTML = "My name is " + myName;
-document.write('canVote is a ' + typeof (canVote) + '<br />');
-document.write('myName is a ' + typeof (myName) + '<br />');
-document.write('anything is a ' + typeof (anything) + '<br />');
+//
+//
+// class Student {
+//     fullName: string;
+//
+//     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+//         this.fullName = `${firstName} ${middleInitial} ${lastName}`
+//     }
+// }
+//
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+//
+// function greeter(person: Person) {
+//     return `Hello, ${person.firstName} ${person.lastName}`;
+// }
+//
+// let user = new Student('Dominik', 'M', 'Palacz'); //{firstName: 'Dominik', lastName: 'Palacz'};
+//
+// //document.body.innerHTML = greeter(user); OK ;)
+//
+// const myName: string = "Dominik";
+// let myAge: number = 41;
+// let canVote: boolean = true;
+// let anything: any = 'dog';
+// anything = 2;
+//
+// document.getElementById("tsStuff").innerHTML = `My name is ${myName}`;
+// document.write('canVote is a ' + typeof (canVote) + '<br />');
+// document.write('myName is a ' + typeof (myName) + '<br />');
+// document.write('anything is a ' + typeof (anything) + '<br />');
+//
+//
+// let strToNum:number = parseInt('5');
+// let numToStr: number = 5;
+//
+// document.write('numToStr is a ' + typeof (numToStr.toString()) + '<br />');
 // });
--- greeter.ts
+++ greeter.ts
@@ -9,41 +9,44 @@
 //
 // document.addEventListener("DOMContentLoaded", function (event) {
 //     console.log("DOM fully loaded and parsed");
-
-
-class Student {
-    fullName: string;
-
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = `${firstName} ${middleInitial} ${lastName}`
-    }
-}
-
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-function greeter(person: Person) {
-    return `Hello, ${person.firstName} ${person.lastName}`;
-}
-
-let user = new Student('Dominik', 'M', 'Palacz'); //{firstName: 'Dominik', lastName: 'Palacz'};
-
-//document.body.innerHTML = greeter(user); OK ;)
-
-const myName: string = "Dominik";
-let myAge: number = 41;
-let canVote: boolean = true;
-let anything: any = 'dog';
-anything = 2;
-
-document.getElementById("tsStuff").innerHTML = `My name is ${myName}`;
-document.write('canVote is a ' + typeof (canVote) + '<br />');
-document.write('myName is a ' + typeof (myName) + '<br />');
-document.write('anything is a ' + typeof (anything) + '<br />');
-
-
-
+//
+//
+// class Student {
+//     fullName: string;
+//
+//     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+//         this.fullName = `${firstName} ${middleInitial} ${lastName}`
+//     }
+// }
+//
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+//
+// function greeter(person: Person) {
+//     return `Hello, ${person.firstName} ${person.lastName}`;
+// }
+//
+// let user = new Student('Dominik', 'M', 'Palacz'); //{firstName: 'Dominik', lastName: 'Palacz'};
+//
+// //document.body.innerHTML = greeter(user); OK ;)
+//
+// const myName: string = "Dominik";
+// let myAge: number = 41;
+// let canVote: boolean = true;
+// let anything: any = 'dog';
+// anything = 2;
+//
+// document.getElementById("tsStuff").innerHTML = `My name is ${myName}`;
+// document.write('canVote is a ' + typeof (canVote) + '<br />');
+// document.write('myName is a ' + typeof (myName) + '<br />');
+// document.write('anything is a ' + typeof (anything) + '<br />');
+//
+//
+// let strToNum:number = parseInt('5');
+// let numToStr: number = 5;
+//
+// document.write('numToStr is a ' + typeof (numToStr.toString()) + '<br />');
 
 // });
